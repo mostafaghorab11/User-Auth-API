4b86eeb58cce0347d850c79d1746dd400216ea4b
Mostafa Ghorab <vetghorab@gmail.com>
2024-04-22 17:23:12 +0200

    jwt-passport is added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const User = require("../models/user");
 const handleValidationError = require("../errors/validation-error");
+const passport = require("../config/passport");
 
 const signup = async (req, res, next) => {
   // console.log(req.body);
@@ -30,9 +34,18 @@ const signup = async (req, res, next) => {
     }
 
     const savedUser = await user.save();
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: savedUser });
+
+    // Generate JWT upon successful signup
+    const payload = { userId: user._id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: "30d",
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: savedUser,
+      token: token,
+    });
   } catch (error) {
     if (error.name === "ValidationError") {
       const formattedErrors = handleValidationError(error);
@@ -50,4 +63,12 @@ const login = (req, res, next) => {
   });
 };
 
-module.exports = { signup, login };
+const dashboard = (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+
+  res.status(200).json({
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = { signup, login, dashboard };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { login, signup } = require("../controllers/auth");
+const { login, signup, dashboard } = require("../controllers/auth");
+
+const passport = require('../config/passport');
 
 const router = express.Router();
 
 router.route("/signup").get().post(signup);
 router.route("/login").get().post(login);
+router
+  .route("/dashboard")
+  .get(passport.authenticate("jwt", { session: false }), dashboard);
 
 module.exports = router;
